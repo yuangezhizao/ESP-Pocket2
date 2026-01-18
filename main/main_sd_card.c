9a8deee962e0dc937be12815543a9ae9a3840f08
yuangezhizao-M1Pro <yuangezhizao@icloud.com>
2026-01-19 00:57:05 +0800

    🐛 Bug(main_sd_card.c): 尝试开启 LDO2 以修复 SD SPI 主机驱动程序示例项目

--- main/main_sd_card.c
+++ main/main_sd_card.c
@@ -18,6 +18,9 @@
 #include "sd_pwr_ctrl_by_on_chip_ldo.h"
 #endif
 
+#include "i2c_driver.h"
+#include "axp202_port.h"
+
 #define EXAMPLE_MAX_CHAR_SIZE 64
 
 static const char *TAG = "MAIN-SD_CARD";
@@ -97,6 +100,19 @@ static esp_err_t s_example_read_file(const char *path)
 
 void app_main(void)
 {
+#if CONFIG_I2C_COMMUNICATION_METHOD_BUILTIN_RW || CONFIG_I2C_COMMUNICATION_METHOD_CALLBACK_RW
+
+    // ESP_ERROR_CHECK(i2c_drv_init());
+
+    ESP_LOGI(TAG, "I2C initialized successfully");
+
+    ESP_ERROR_CHECK(axp202_init());
+
+    // Run bus scan
+    i2c_drv_scan();
+
+#endif
+
     esp_err_t ret;
 
     // Options for mounting the filesystem.
